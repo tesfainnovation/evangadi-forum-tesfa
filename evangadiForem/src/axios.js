173a428b67385f4c467f21702a7b95825142ff49
TomYenialem <tom21g2008@gmail.com>
2024-12-30 18:09:14 +0300

    deploy the server side

--- evangadiForem/src/axios.js
+++ evangadiForem/src/axios.js
@@ -3,9 +3,10 @@ import axios from 'axios'
 
 
 
-const api=axios.create({
-    baseURL:'http://localhost:3000/Api'
-})
+const api = axios.create({
+  // baseURL:'http://localhost:3000/Api'
+  baseURL: "https://eva-deploy-3.onrender.com",
+});
  
 
 
